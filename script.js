0ad8f9edd1ac2214cce02aceee0996879b10cac4
Abyss14 <110331132+Abyss14@users.noreply.github.com>
2022-08-19 01:07:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,4 @@ $(document).ready(function () {
         
     });
 });
-$(document).ready(function () {
-    $('.slider_2').bxSlider1({
-        pagerCustom: '.slider-nav',
-        auto: true,
-        autoCotrols: false,
-        infiniteLoop: true,
-        controls:false,
-        pause:3000,
-        easing: "swing",
-        speed: 1500,
-        slideMargin: 300,
-        nextText: '',
-        prevText: '',
-        
-        
-    });
-});
+
